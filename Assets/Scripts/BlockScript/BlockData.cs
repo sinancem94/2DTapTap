24a3763c38f10188e684ff2a9da303254e954669
Sinan Cem Çaylı <sinancemcayli@Sinan-MBP-2.mynet>
2019-02-03 10:39:24 +0300

    sizeFitter

--- Assets/Scripts/BlockScript/BlockData.cs
+++ Assets/Scripts/BlockScript/BlockData.cs
@@ -7,6 +7,9 @@ public class BlockData
     public static Color normalColor = new Color32(159, 77, 77, 255);
     public static Color reverseColor = new Color32(75, 170, 120, 255);
 
+    public static Sprite normalBlock = Resources.Load<Sprite>("Sprites/NormalBlock");//, typeof(Texture)) as Texture;
+    public static Sprite reverseBlock = Resources.Load<Sprite>("Sprites/ReverseBlock");
+
     public enum blockType
     {
         normal,
@@ -19,11 +22,13 @@ public class BlockData
         if (type == blockType.normal)
         {
             type = blockType.reverse;
+            blockSprite.sprite = reverseBlock;
             blockSprite.color = reverseColor;
         }
         else
         {
             type = blockType.normal;
+            blockSprite.sprite = normalBlock;
             blockSprite.color = normalColor;
         }
     }
--- Assets/Scripts/BlockScript/BlockType.cs
+++ Assets/Scripts/BlockScript/BlockType.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockType : MonoBehaviour {
 
     public BlockData.blockType type;
-    public SpriteRenderer blockSprite;
+    private SpriteRenderer blockSprite;
+    private Text limitText;
+
+    public int limit;
 
     private void Start()
     {
         blockSprite = GetComponent<SpriteRenderer>();
-        
-        type = BlockData.blockType.normal;
+        blockSprite.sprite = BlockData.normalBlock;
         blockSprite.color = BlockData.normalColor;
+
+        limit = 1;
+
+        type = BlockData.blockType.normal;
+
     }
 
     public void ChangeType()
     {
         BlockData.ChangeBlockType(ref type, blockSprite);
+    }
+
+    public void ChangeLimit(int num)
+    {
+        limit += num; 
     }
 }
